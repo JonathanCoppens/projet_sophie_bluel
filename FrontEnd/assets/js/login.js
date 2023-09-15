cd760a968e1783e3c24ee42b65a8a075101d8f71
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-09-15 08:34:24 +0200

    token saved and set in gitignore

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,73 +1,7 @@
-"use strict";
+const token = process.env.TOKEN; //récupération du token inséré dans la console
 
-const url = "http://localhost:5678/api/users/login";
-
-var email = localStorage.getItem("email");
-var password = localStorage.getItem("password");
-
-const btn =  document.getElementById('submit-log-btn');
-
-btn.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    // Récup  des données du formulaire
-
-    fetch(url) //Vérification adresse serveur
-    .then (response => response.json()) //Récupération info serveur
-    .then (data => {
-        console.log(data); // Data doit représenter le token émis par le serveur
-
-        
-
-    });
-
+const request = new Request(url, {
+    headers: {
+        Authorization:`Bearer ${token}`,
+    },
 });
-
-/*******/
-
-/*    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY4OTU4MzY1MSwiZXhwIjoxNjg5NjcwMDUxfQ.xdvwYjLFwUth24oZcIP8Lhsy8303kNAd6AXz40RcqEs";
-
-    localStorage.setItem("token", token);
-
-    const myHeaders = new Headers ({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer' + token,
-    });
-
-    fetch('http://localhost:5678/api/users/login', {
-        method:'GET',
-        headers: myHeaders,
-    })
-
-    .then (response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-
-    .catch(error => {
-        console.log('Une erreur s\'est produite', error);
-    });
-
-import {value} from "./storage.json";
-value(userID, token, email, password)
-    .then(() => {
-        console.log('Upload réussis');
-    })
-    .catch((error) => {
-        console.log('Erreur lors de l\'upload', error);
-    }); */
-
-    /*****/
-
-    document.getElementById("login").addEventListener("submit", function (e) {
-        e.preventDefault();
-
-        var xhr = new XMLHttpRequest ();
-        xhr.onreadystatechange = function () {
-            console.log(this);
-        };
-
-        xhr.open("GET", "/async/script")
-
-        return false;
-    });
