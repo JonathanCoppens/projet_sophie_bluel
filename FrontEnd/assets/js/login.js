c16dee643649cd9eef463c8421a1742cdb5521f3
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-09-18 11:24:46 +0200

    ajout filtre page accueil + js login

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,7 +1,63 @@
-const token = process.env.TOKEN; //récupération du token inséré dans la console
+/* // .env gitignore comment récupérer le token ? pour ne pas afficher les données sensibles
+const data = `{
+    "userId": 1,
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NDc1ODQ1NywiZXhwIjoxNjk0ODQ0ODU3fQ.7XlBR7cF3NEyXIcNRGQNT1ojoXPV6Py1VSLLYhqMUzM"
+}`;
 
-const request = new Request(url, {
+const jsonString = JSON.stringify(data); // mise en chaîne de caractère desdonnées JSON
+
+const fs = require("fs");
+
+fs.writeFile("./credentials.json", jsonString, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Le fichier a été créé");
+    }
+})
+
+const parseData = JSON.parse(data);
+
+const token = parseData.token;
+
+const url = "http://localhost:5678/api-docs/";
+
+const request = fetch(url);
+
+request.then((response) => {
+    if (response.status === 200) {
+        const data = response.json();
+        
+        console.log("Connected");
+    }
+    if (response.status === 401) {
+        console.log("Not Authorized");
+    } 
+    if (response.status === 404) {
+        console.log("User not found");
+    }
+}) */
+
+
+
+const data = {
+    email: "sophie.bluel@test.tld",
+    password: "S0phie"
+  };
+  
+  const response = await fetch("/users", {
+    method: "POST",
     headers: {
-        Authorization:`Bearer ${token}`,
+      "Content-Type": "application/json"
     },
-});
+    body: JSON.stringify(data)
+  });
+  
+  if (response.status === 200) {
+    const data = response.json();
+    console.log("Connected");
+  } else if (response.status === 401) {
+    console.log("Not Authorized");    
+  } else (response.status === 404) {
+    console.log("User not found");
+  }
