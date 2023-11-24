302f514a28161fdd76746f664d597bec614de455
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-11-24 10:20:16 +0100

    overlay added on main page

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,9 +1,26 @@
 const icoModif = document.querySelector(".ico-modif");
 
-const modal = document.querySelector(".modal-btn open-modal");
+const modal = document.querySelector(".modal-btn");
+//console.log(modal.value);
 
-icoModif.addEventListener("click", () => {
-    //console.log("modal");
+modal.addEventListener("click", () => {
+    const body = document.body;
+    // new div to create an overlay for modal
+    const overlay = document.createElement('div');
+    // create the class to use it in the DOM
+    overlay.classList.add('modal-overlay');
+    // style for positionning size and color
+    overlay.style.backgroundColor = '#f0f0f0';
+    overlay.style.position = 'fixed';
+    overlay.style.top = "0";
+    overlay.style.left =" 0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.opacity = "0.5";
+    // disable current page
+    body.style.pointerEvents = 'none';
 
-    
+    body.appendChild(overlay);
+
+    modal.style.display = 'block';
 })
