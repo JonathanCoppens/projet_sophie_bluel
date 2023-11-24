ff121d9b4ecbbaf0d8222bf2985a3239bce907de
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-11-24 09:29:42 +0100

    start js modal

---
+++ FrontEnd/assets/js/modal.js
@@ -0,0 +1,9 @@
+const icoModif = document.querySelector(".ico-modif");
+
+const modal = document.querySelector(".modal-btn open-modal");
+
+icoModif.addEventListener("click", () => {
+    //console.log("modal");
+
+    
+})
