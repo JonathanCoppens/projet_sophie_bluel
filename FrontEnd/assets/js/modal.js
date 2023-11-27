8f231693652d4313c8d50b477e62449e4b3c2b60
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-11-27 09:05:52 +0100

    creation overlay, body and close button'modal

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,26 +1,23 @@
-const icoModif = document.querySelector(".ico-modif");
-
 const modal = document.querySelector(".modal-btn");
 //console.log(modal.value);
 
 modal.addEventListener("click", () => {
     const body = document.body;
     // new div to create an overlay for modal
+    const closeBtn = document.createElement("button");
+    const bodyModal = document.createElement("div");
     const overlay = document.createElement('div');
     // create the class to use it in the DOM
+    closeBtn.classList.add('close-modal');
+    bodyModal.classList.add('body-modal');
     overlay.classList.add('modal-overlay');
-    // style for positionning size and color
-    overlay.style.backgroundColor = '#f0f0f0';
-    overlay.style.position = 'fixed';
-    overlay.style.top = "0";
-    overlay.style.left =" 0";
-    overlay.style.width = "100%";
-    overlay.style.height = "100%";
-    overlay.style.opacity = "0.5";
-    // disable current page
-    body.style.pointerEvents = 'none';
 
+    closeBtn.innerText = 'X';
+
+    bodyModal.appendChild(closeBtn);
+    overlay.appendChild(bodyModal);
     body.appendChild(overlay);
 
-    modal.style.display = 'block';
+    //modal.style.display = 'block';
 })
+
