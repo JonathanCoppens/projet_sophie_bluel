c8346e10ab7c5ffb33813dae9046d27883c6fe31
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-12-19 08:45:35 +0100

    js reorganization by function but not ok

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,63 +1,124 @@
 const modifBtn = document.querySelector('.open-modal');
 
 // open modal function
+
+modifBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    
+    generateModal();
+});
+    
 function generateModal() {
-    modifBtn.addEventListener('click', () => {
+    
+    const close = document.querySelector('.close-btn');
+    close.addEventListener('click', closeModal);
+    modifBtn.removeEventListener('click', generateModal);
+
+    const title = document.querySelector('.title');
+    title.innerText = 'Gallerie photo';
 
         // modal header
-        const modalOverlay = document.querySelector('.modal-overlay');
-        modalOverlay.style.display = 'flex'; //déplacer
 
-        const modal = document.querySelector('.modal');
-
-        const modalHeader = document.querySelector('.modal-header');
+        const Header = document.createElement('div');
+              Header.classList.add('modal-header');
         
-        const close = document.querySelector('.close-btn');
-        close.addEventListener('click', closeModal);
-        modifBtn.removeEventListener('click', generateModal);
-
-        const arrow = document.querySelector('.arrow');
-        arrow.style.display = 'none';
-
-        const title = document.querySelector('.title');
-        title.innerText = 'Gallerie photo';
-
         // modal body
-        const modalBody = document.querySelector('.modal-body');
-        
-        generateGalleryModal();
-        // modal footer
-        const modalFooter = document.querySelector('.modal-footer')
-        const line = document.querySelector('hr');
 
-        const addBtn = document.querySelector('.add-btn');
-        addBtn.innerText = 'Ajouter une photo'
+        const Body = document.createElement('div');
+              Body.classList.add('modal-body');
         
-        modal.appendChild(modalHeader);
-        modal.appendChild(modalBody);
-        modal.appendChild(modalFooter);
-        modalOverlay.appendChild(modal);
-    });
-}
+        // modal footer
+
+        const Footer = document.createElement('div');
+              Footer.classList.add('modal-footer');
+
+        const modal = document.createElement('div');
+              modal.classList.add('modal');
+              modal.appendChild(Header);
+              modal.appendChild(Body);
+              modal.appendChild(Footer);
+
+        //modal header
+        const overlay = document.createElement('div');
+              overlay.classList.add('modal-overlay');
+              overlay.style.display = 'flex';
+              overlay.appendChild(modal);
+              overlay.addEventListener('click', event => {
+
+                if (event.target.classList.constains('modal-overlay')) {
+                    closeModal();
+                }
+              });
+
+        document.body.append(overlay);
+};
 
 function generateGalleryModal() {
-    const modalGallery = document.querySelector('.modal-gallery');
+    generateModal();
 
-    while (modalGallery.firstChild) {
-        modalGallery.removeChild(modalGallery.firstChild);
-    }
+    // modal header
+    
+    const closeBtn = document.createElement('button');
+    closeBtn.classList.add('close-btn');
+    closeBtn.innerHTML = '&times;';    
+    closeBtn.addEventListener('click', closeModal);
+          
+    const header = document.querySelector('.modal-header');
+          header.innerText = 'Galerie photo';
+          header.append(closeBtn);
+
+    // footer
+
+    const addBtn = document.createElement('button');
+          addBtn.classList.add('add-btn');
+          addBtn.innerText = 'Ajouter une photo';
+          addBtn.addEventListener('click', event => {
+            closeModal();
+            generateNewWorkModal();
+          });
+
+    const footer = document.querySelector('.modal-footer');
+          footer.append(addBtn);
+    
+    // modal body
+
+    const gallery = document.querySelector('div');
+          gallery.classList.add('modal-gallery');
+
     works.forEach(work => {
+        const image = document.createElement('image');
+              image.src = work.imageUrl;
+              image.alt = work.title;
+        
         const figure = document.createElement('figure');
-        const image = document.createElement('img');
-        image.src = work.imageUrl;
-        image.alt = work.title;
+              figure.appendChild(image);
 
-        figure.appendChild(image);
-        modalGallery.appendChild(figure);
+              gallery.appendChild(figure);
     });
+
+    const body = document.querySelector('.modal-body');
+          body.append(gallery);
+}
+
+function generateNewWorkModal () {
+    
+    generateModal();
+
+    // modal header
+    const closeBtn = document.createElement('button');
+          closeBtn.classList;add()
+          closeBtn.innerHTML = '&times;';
+          closeBtn.addEventListener('click', closeModal);
+
+    const header = document.querySelector('.modal-header');
+          header.innerText = 'Ajout photo';
+          header.append(closeBtn);
+
 }
 
 // close modal function
+
 function closeModal() {
     const modalOverlay = document.querySelector('.modal-overlay');
     modalOverlay.style.display = 'none';
@@ -72,12 +133,3 @@ function closeModal() {
     
     modifBtn.addEventListener('click', generateModal);
 }
-
-function addPicture() {
-    const addBtn = document.querySelector('.add-btn');
-    addBtn.addEventListener('click', () => {
-        bodyModal();
-    });
-}
-
-generateModal();
