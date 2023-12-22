c7e1f1a2c4fb24ff9b7095767680139179920225
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-12-22 01:30:21 +0100

    add element to 2nd modal in css & modal.js

--- FrontEnd/assets/js/modal-bis.js
+++ FrontEnd/assets/js/modal-bis.js
@@ -4,8 +4,6 @@ modifBtn.addEventListener('click', (event) => {
     event.preventDefault();
     event.stopImmediatePropagation();
 
-    //generateModal();
-
     generateModal('Gallerie photo', (body, footer) => {
         generateGalleryModal(body, footer);
     });
@@ -26,37 +24,16 @@ function generateModal(title, contentGenerator) {
     closeBtn.classList.add('close-btn');
     closeBtn.innerHTML = '&times;';
     closeBtn.addEventListener('click', closeModal);
-
-    //modifBtn.removeEventListener('click', generateModal);
     Header.appendChild(closeBtn);
 
     const modalTitle = document.createElement('h3');
     modalTitle.innerText = title;
     Header.appendChild(modalTitle);
 
-    /* const title = document.createElement('h3');
-    title.innerText = 'Gallerie photo';
-    Header.appendChild(title); */
-
     // Modal body
 
     const Body = document.createElement('div');
     Body.classList.add('modal-body');
-
-    /* const gallery = document.createElement('div');
-    gallery.classList.add('modal-gallery'); */
-
-    /* works.forEach(work => {
-        const image = document.createElement('img');
-        image.src = work.imageUrl;
-        image.alt = work.title;
-
-        const figure = document.createElement('figure');
-        figure.appendChild(image);
-        gallery.appendChild(figure);
-    });
-
-    Body.appendChild(gallery); */
 
     // Call the funtion to generate specific modal content
 
@@ -64,10 +41,6 @@ function generateModal(title, contentGenerator) {
 
     const Footer = document.createElement('div');
     Footer.classList.add('modal-footer');
-
-    /* const hrDiv = document.createElement('div');
-    hrDiv.classList.add('hr-container');
-    Footer.appendChild(hrDiv); */
 
     const line = document.createElement('hr');
     line.classList.add('grey-line');
@@ -97,15 +70,10 @@ function generateModal(title, contentGenerator) {
 }
 
 function generateGalleryModal(body, footer) {
-    //generateModal();
 
     // Modal header
 
     const header = document.querySelector('.modal-header');
-
-    /* const title = document.createElement('div');
-    title.innerText = 'Galerie photo';
-    header.appendChild(title); */
 
     // Modal footer
     const addBtn = document.createElement('button');
@@ -113,10 +81,9 @@ function generateGalleryModal(body, footer) {
     addBtn.innerText = 'Ajouter une photo';
     addBtn.addEventListener('click', () => {
         closeModal();
-        //generateNewWorkModal();
+        generateNewWorkModal();
     });
 
-    //const footer = document.querySelector('modal-footer');
     footer.appendChild(addBtn);
 
     // Modal body
@@ -136,22 +103,99 @@ function generateGalleryModal(body, footer) {
     });
 
     body.appendChild(gallery);
-    /* const title = document.createElement('h3');
-    title.classList.add('title');
-    title.innerText = 'Ajout photo';
-    header.appendChild(title); */
-
-    /* const header = document.querySelector('.modal-header');
-    header.innerText = 'Modal gallery';
-    header.appendChild(closeBtn); */
-
-    /* const footer = document.createElement('div');
-    footer.classList.add('modal-footer');
-    footer.appendChild(addBtn);
-    
-    paretnElement.appendChild(footer); */
 }
 
+function generateNewWorkModal() {
+    generateModal('Ajout photo', (body, footer) => {
+
+        // Modal header
+
+        const header = document.createElement('div');
+        header.classList.add('modal-header');
+
+        const back = document.createElement('button');
+        back.classList.add('back-arrow');
+        back.src = './assets/icons/fleche-gauche.png';
+        header.appendChild(back);
+
+        const title = document.createElement('h3');
+        title.innerText = 'Ajout photo';
+        header.appendChild(title);
+
+        // Modal body
+
+        /* const bodyContent = document.createElement('div');
+        bodyContent.classList.add('modal-body'); */
+
+        // Add container
+
+        const addContainer = document.createElement('div');
+        addContainer.classList.add('add-container');
+        body.appendChild(addContainer);
+
+        const picIcon = document.createElement('img');
+        picIcon.classList.add('icon');
+        picIcon.src = './assets/icons/logo-img.png'
+        addContainer.appendChild(picIcon);
+        
+        const addPic = document.createElement('button');
+        addPic.classList.add('add-pic');
+        addPic.innerText = '+ Ajouter photo';
+        addContainer.appendChild(addPic);
+
+        const addDetails = document.createElement('p');
+        addDetails.classList.add('add-details');
+        addDetails.innerText = 'jpg, png, : 4Mo max';
+        addContainer.appendChild(addDetails);
+
+        // Modal form
+
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('form-div');
+        body.appendChild(formContainer);
+
+        const form = document.createElement('form');
+        formContainer.appendChild(form);
+
+        const labelInput = document.createElement('label');
+        labelInput.innerText = 'Titre';
+        form.appendChild(labelInput);
+
+        const formInput = document.createElement('input');
+        form.appendChild(formInput);
+
+        const labelSelect = document.createElement('label');
+        labelSelect.innerText = 'Catégorie';
+        form.appendChild(labelSelect);
+
+        const selectForm = document.createElement('select');
+        selectForm.classList.add('select');
+        
+        let options = ['Objets', 'Appartements', 'Hôtels & restaurants'];
+        for (let i = 0; i < options.length; i++) {
+            let optionElement = document.createElement('option');
+            optionElement.text = options[i];
+            optionElement.value = 'option' + (i + 1);
+            selectForm.appendChild(optionElement);
+
+            console.log(options[i]);
+        }
+
+        form.appendChild(selectForm);
+
+        // add-container
+        // form
+
+        // Modal footer
+
+        
+        /* const footerContent = document.createElement('div');
+        footerContent.classList.add('modal-footer'); */
+
+        /* body.appendChild(bodyContent);
+        footer.appendChild(footerContent); */
+    });
+}
 
 function closeModal() {
     const modalOverlay = document.querySelector('.modal-overlay');
